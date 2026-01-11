5d99a7f48b86a7e4349e98d70d1c4de8995626a6
paldiyanamrata70 <paldiyanamrata@gmail.com>
2026-01-11 10:39:11 +0530

    Change splash logo to kosha logo.png

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Splash = () => {
   return (
     <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
-      <img src="/pictures/logo.jpg" alt="Kosha Logo" className="logo-fade-in max-w-xs h-auto" />
+      <img src="/pictures/kosha logo.png" alt="Kosha Logo" className="logo-fade-in max-w-xs h-auto" />
     </div>
   );
 };
