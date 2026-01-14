5579d87628abe7b65c8496121e77f07c35962214
paldiyanamrata70 <paldiyanamrata@gmail.com>
2026-01-14 16:41:51 +0530

    Updated footer layout: adjusted margins, added Pinterest to mobile, refined spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,12 +37,17 @@ I admire your design aesthetic and would love updates on new launches, collectio
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                 </svg>
               </a>
+              <a href="#" aria-label="Pinterest">
+                <svg className="w-8 h-8 fill-black" viewBox="0 0 24 24">
+                  <path d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146 1.124.347 2.317.535 3.554.535 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z" />
+                </svg>
+              </a>
             </div>
           </div>
         </div>
 
         {/* Desktop: Stay Updated */}
-        <div className="footer-col hidden md:block">
+        <div className="footer-col hidden md:block ml-10">
           <h4>STAY UPDATED</h4>
           <input type="tel" placeholder="Enter your mobile number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="subscribe-input bg-transparent border-b-2 border-black text-purple-300 px-3 py-2 mb-4 placeholder:text-[#606060] w-3/4" />
           <p className="mb-4">*Kosha does not share data with any third parties*</p>
@@ -50,7 +55,7 @@ I admire your design aesthetic and would love updates on new launches, collectio
         </div>
 
         {/* Contact Us */}
-        <div className="footer-col mx-auto md:ml-16">
+        <div className="footer-col mx-auto md:ml-24">
           <h4>CONTACT US</h4>
           <p>
             <Phone className="inline-block w-4 h-4 mr-4" />
@@ -71,7 +76,7 @@ I admire your design aesthetic and would love updates on new launches, collectio
         </div>
 
         {/* Desktop: Follow Us */}
-        <div className="footer-col hidden md:block ml-16">
+        <div className="footer-col hidden md:block ml-24">
           <h4>FOLLOW US</h4>
           <div className="social-icons">
             <a href="https://www.instagram.com/kosha_fabric_artistry?igsh=OW4zZWZhaGxmdDNl" aria-label="Instagram">
