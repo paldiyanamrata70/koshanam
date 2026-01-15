8cf817f8f33070fda141293ba2dc7e350627ea4e
paldiyanamrata70 <paldiyanamrata@gmail.com>
2026-01-15 05:34:28 +0530

    Update footer background to deep maroon, adjust text and icons to white, add spacing adjustments in mobile view

--- src/components/WatchFinest.tsx
+++ src/components/WatchFinest.tsx
@@ -49,7 +49,7 @@ const WatchFinest = () => {
   }, []);
 
   return (
-    <div id="finest-film" className="watch-finest" ref={componentRef} style={{ marginTop: window.innerWidth <= 768 ? '-30px' : '-10px' }}>
+    <div id="finest-film" className="watch-finest mb-[38px]" ref={componentRef} style={{ marginTop: window.innerWidth <= 768 ? '-30px' : '-10px' }}>
       <h2 className="watch-title">Watch Finest</h2>
       <div className="scroll-wrapper">
         <button className="finest-scroll-btn left" onClick={() => scroll(-1)}>
