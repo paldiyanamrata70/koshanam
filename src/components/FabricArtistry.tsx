ac19df72b8f70658d20f1725a7d7d0a3ae7ae27f
paldiyanamrata70 <paldiyanamrata@gmail.com>
2026-01-20 10:16:20 +0530

    Update fabric8 path to .JPG for visibility

--- src/components/FabricArtistry.tsx
+++ src/components/FabricArtistry.tsx
@@ -16,7 +16,7 @@ const FabricArtistry = () => {
 
   const fabrics = [
     '/pictures/fabric1.JPG', '/pictures/fabric2.jpg','/pictures/fabric3.jpg',
-     '/pictures/fabric5.jpg','/pictures/fabric8.png' , '/pictures/fabric6.jpg',
+     '/pictures/fabric5.jpg','/pictures/fabric8.JPG' , '/pictures/fabric6.jpg',
     '/pictures/fabric7.jpg', '/pictures/fabric9.jpg','/pictures/fabric11.JPG',
      '/pictures/fabric10.jpg', '/pictures/fabric4.JPG','/pictures/fabric12.JPG',
   ];
